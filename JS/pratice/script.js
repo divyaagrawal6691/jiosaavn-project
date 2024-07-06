6198e591ccfda3b9165101fec9bd103d382f3837
divyaagrawal6691 <agrawaldivya6691@gmail.com>
2024-07-07 03:22:53 +0530

    first commit

---
+++ JS/pratice/script.js
@@ -0,0 +1,69 @@
+//why we learn programming?
+//case sensitive language- x. X
+//byte code - 0.1
+//data type - num, string, undefined, boolean, null
+//variable - variable is a container which stores data/value.
+//will not start with numeric vhr, special chr/
+
+// let x = 20;
+// let y = 10;
+// let result =x + y;
+// console.log(x + y);
+
+// let age = 63;
+// if(age >= 60){
+//     console.log("person is senior citizen");
+// }
+// else{
+//     console.log("person is not senior citizen");
+// }
+
+// let num1= 20;
+// let num2=30;
+// if(num1>=num2){
+//     console.log("num1 is greater");
+// }
+// else{
+//     console.log("num2 is greater");
+// }
+
+// let num1=20;
+// let num2=30;
+// let num3=10;
+// if(num1>num2&&num1>num3){
+//     document.write("num1 is graeter");
+// }
+// else if(num2>num1&&num2>num3){
+//     document.write("num2 is graeter");
+// }
+// else if(num3>num1&&num3>num2){
+//     document.write("num3 is graeter");
+// }
+
+// let num1=0;
+// let num2=30;
+// let num3=10;
+// if(num1<num2&&num1<num3){
+//     document.write("num1 is smallest");
+// }
+// else if(num2<num1&&num2<num3){
+//     document.write("num2 is smallest");
+// }
+// else if(num3<num1&&num3<num2){
+//     document.write("num3 is smallest");
+// }
+
+//create multiple objects same method(key) id,fullname,class,age
+
+// let student1 = new Object();
+// let student2 = new Object();
+// student1.id ="11";
+// student1.fullname ="agrawal";
+// student1.class ="2nd";
+// student1.age="21";
+// student1.id ="1";
+// student1.fullname ="agrawal";
+// student1.class ="2nd";
+// student1.age="21";
+// console.log(student1);
+// console.log(student2);
