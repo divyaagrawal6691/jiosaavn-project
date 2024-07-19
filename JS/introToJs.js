9f0d8514b0adcabe183ab33600315af4e15222cf
divyaagrawal6691 <agrawaldivya6691@gmail.com>
2024-07-19 17:40:06 +0530

    first commit

---
+++ JS/introToJs.js
@@ -0,0 +1,44 @@
+// problem 1
+
+console.log("Ducat an IT Institute")
+console.log("A Transformation in Education")
+console.log("")
+
+
+// problem 2
+
+var name = "Divya Agrawal";
+console.log(name)
+name ="Mr. Rajeev Agrawal";
+console.log(name)
+name ="Mrs. Sangeeta Agrawal";
+console.log(name)
+console.log("")
+
+// problem 3
+
+var name ="Divya Agrawal";
+var age= "21";
+console.log(name, age);
+console.log("string      ", "number")
+console.log("")
+
+// problem 4
+
+
+console.log("***-***-***-***-***-***-***-***-***-***-***-***-***-***-")
+console.log("|           Hem Vidya Niketan School Khair             |")
+console.log("|                    (Report Card)                     |")
+console.log("|*--**--**--**--**--**--**--**--**--**--**--**--**--**-|")
+console.log("|   Name       :       Divya Agrawal                   |")
+console.log("|   School     :       Hem Vidya Niketan School        |")
+console.log("|   Grade      :       A++                             |")
+console.log("|   Section    :       A                               |")
+console.log("|   Roll no.   :       13                              |")
+console.log("|*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-|")
+console.log("|   Marks :-                                           |")
+console.log("|   Science    :       98                              |")
+console.log("|   Maths      :       96                              |")
+console.log("|   Hindi      :       99                              |")
+console.log("***-***-***-***-***-***-***-***-***-***-***-***-***-***-")
+
---
+++ JS/pratice/introductiontoJavascriptVariables.js
@@ -0,0 +1,44 @@
+//Javascript Variable: it is like a container/box which is used to store technical data(numbers, strings)
+
+//numbers: integer number(negative(-1,-34), and postive(1,2,3,4)), decimal number(point numebr: 3459.34, 19982.99)
+//words(strings): set of characters(numbers + letter + special symbols) enclosed with single and double quatation marks.
+//Examples of string: "rakesh", 'r', "8700459092", "201301", "rakesh1234", "Hey, I'm anand from jhansi, actually i have some questions regarding your frontend courses."
+
+//Note: Declaration is also known as creating/making something.
+
+//Decalartion of variable: 'var' keyword - it is used to create variable in javascript
+
+var pictures
+var age 
+var name
+var pincode
+
+//you've created some variable above with names: pictures, age, name and pincode, once you've not given any value to above variables then by default your compiler give them undefined(you've not defined value, no value) as value.
+
+//how to check what above variables are containing: console.log(variableName): it is like a tool which is used to print data inside particular variable.
+
+console.log(pictures)
+console.log(age)
+console.log(name)
+console.log(pincode)
+
+//how to add data to the created variable: = (assignment operator/sign): assign/add values to the variable.
+
+//Note: Initialization is also known as adding/inserting something.
+
+//Intialization of variable.
+
+/*
+syntax: how to use paticular thing in javascript
+varibleName = value
+*/
+
+//below you're initializing/inserting/adding the value to the created variable.
+// pictures = "Nature Picture"
+// console.log(age) //undefined
+// age = 34
+// name = "Ducat"
+// pincode = "201307"
+
+// console.log(pictures) //Nature Picture
+// console.log(age) //34
